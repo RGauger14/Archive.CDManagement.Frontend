154eb63f8751091a28c0996c74d62ccc40d3a608
Ryan Gauger <rgauger.dev@gmail.com>
2020-09-16 16:55:19 +1000

    Added CD model, Repo and Pages

---
+++ src/Models/CDModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Archive.CDManagement.Frontend.Models
+{
+    public class CDModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Section { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Barcode { get; set; }
+
+        public string Description { get; set; }
+
+        public bool OnLoan { get; set; }
+    }
+}
---
+++ src/Repositories/Abstractions/ICDRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Archive.CDManagement.Frontend.Repositories.Abstractions
+{
+    public interface ICDRepository
+    {
+        
+    }
+}
