94795916dc5d40581da4dce21a07a95822b7e2c7
Ryan Gauger <rgauger.dev@gmail.com>
2020-10-06 19:22:10 +1000

    fixed typo on CDs Page.  Added Bindproperty to Pages/Rentals/Edit. Added OnPostRemove function.

--- src/Pages/Rentals/Edit.cshtml.cs
+++ src/Pages/Rentals/Edit.cshtml.cs
@@ -2,14 +2,17 @@ using System.Collections.Generic;
 using System.Linq;
 using Archive.CDManagement.Frontend.Models;
 using Archive.CDManagement.Frontend.Repositories.Abstractions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Archive.CDManagement.Frontend.Pages.Rentals
 {
-    public class EditModel : PageModel 
+    public class EditModel : PageModel
     {
         private readonly IRentalRepository _rentalRepository;
         private readonly ICDRepository _cDRepository;
+
+        [BindProperty]
         public RentalModel Rental { get; set; }
         public List<CDModel> CDs { get; set; }
 
@@ -17,12 +20,19 @@ namespace Archive.CDManagement.Frontend.Pages.Rentals
         {
             _rentalRepository = rentalRepository;
             _cDRepository = cDRepository;
-            
+
         }
         public void OnGet(int id)
         {
             Rental = _rentalRepository.Read(id);
             CDs = _cDRepository.GetAll().Where(cd => !cd.OnLoan).ToList();
+        }
+
+        public void OnPostRemove(int rentalItemId)
+        {
+            var rentalID = Rental.RentalItems.Single(rentalItem => rentalItem.Id == rentalItemId);
+            Rental.RentalItems.Remove(rentalID);
+            _rentalRepository.Edit(Rental);
         }
     }
 }
